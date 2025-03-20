ee2c0f71f95e995dca90af698e2a4fc341ad98de
anikatasnin <jarintasnin09@gmail.com>
2025-03-20 17:29:58 +0600

    fetched phones data

--- src/components/Phones.jsx
+++ src/components/Phones.jsx
@@ -1,7 +1,10 @@
+import { useLoaderData } from "react-router-dom";
+
 const Phones = () => {
+  const phones = useLoaderData();
   return (
     <div>
-      <h2>Phones here</h2>
+      <h2>Phones here: {phones.length}</h2>
     </div>
   );
 };
